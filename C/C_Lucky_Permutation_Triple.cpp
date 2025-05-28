b34b6414b4dce02e06c956118182146d2eadd3e4
Anshul Saxena <f20221041@hyderabad.bits-pilani.ac.in>
2025-05-28 13:29:45 +0530

    C Contest_304 - C_Lucky_Permutation_Triple

--- C/C_Lucky_Permutation_Triple.cpp
+++ C/C_Lucky_Permutation_Triple.cpp
@@ -1,31 +1,64 @@
-//   ____                  _           _ _       
-//  / __ \  __ _ _ __  ___| |__  _   _| | |  ___ 
+//   ____                  _           _ _
+//  / __ \  __ _ _ __  ___| |__  _   _| | |  ___
 // / / _` |/ _` | '_ \/ __| '_ \| | | | | | / __|
-//| | (_| | (_| | | | \__ \ | | | |_| | | |_\__ 
+//| | (_| | (_| | | | \__ \ | | | |_| | | |_\__
 // \ \__,_|\__,_|_| |_|___/_| |_|\__,_|_|_(_)___/
-//  \____/                                       
+//  \____/
 
-#include<bits/stdc++.h>
+#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll                    long long int
-#define pb                    push_back
-#define vi                    vector<int>
-#define vll                   vector<ll>
-#define vc                    vector<char>
+#define ll long long int
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
 #define fo(i, n) for (long long i = 0; i < n; i++)
 #define s(a) a.size()
-#define c(out) cout<<out<<endl
+#define c(out) cout << out << endl
 
-/*---------------------------------------------------------------CODE BEGINS ->!---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------CODE BEGINS
+ * ->!---------------------------------------------------------------------------*/
 
-void _code(){
+int main() {
+  int n;
+  cin >> n;
 
-}
+  bool diff = true;
+  int a[n], b[n], c[n];
 
-int main(){
-    ll t;
-    cin>>t;
-    while(t--) {
-        _code();
+  if (n > 1) {
+    a[0] = 0;
+    b[0] = 1;
+    c[0] = 1;
+    fo(i, n - 1) {
+      a[i + 1] = i + 1;
+      b[i + 1] = (i + 2) % n;
+      c[i + 1] = (a[i + 1] + b[i + 1]) % n;
     }
-}   
+
+  } else {
+    a[0] = b[0] = c[0] = 0;
+  }
+
+  vector<int> vec(c, c + n);
+  sort(vec.begin(), vec.end());
+  fo(i, n - 1) {
+    if (vec[i] == vec[i + 1]) {
+      diff = false;
+      break;
+    }
+  }
+  if (diff) {
+
+    fo(i, n) cout << a[i] << " ";
+    cout << endl;
+
+    fo(i, n) cout << b[i] << " ";
+    cout << endl;
+
+    fo(i, n) cout << c[i] << " ";
+    cout << endl;
+  } else
+    cout << -1 << endl;
+}
