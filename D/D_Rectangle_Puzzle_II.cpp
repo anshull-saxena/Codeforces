c9d82383087f7030ef58aa15256f19e272fda33d
Anshul Saxena <f20221041@hyderabad.bits-pilani.ac.in>
2025-05-29 18:40:51 +0530

    took hint for D_Rectangle_Puzzle_II

--- D/D_Rectangle_Puzzle_II.cpp
+++ D/D_Rectangle_Puzzle_II.cpp
@@ -1,31 +1,46 @@
-//   ____                  _           _ _       
-//  / __ \  __ _ _ __  ___| |__  _   _| | |  ___ 
+//   ____                  _           _ _
+//  / __ \  __ _ _ __  ___| |__  _   _| | |  ___
 // / / _` |/ _` | '_ \/ __| '_ \| | | | | | / __|
-//| | (_| | (_| | | | \__ \ | | | |_| | | |_\__ 
+//| | (_| | (_| | | | \__ \ | | | |_| | | |_\__
 // \ \__,_|\__,_|_| |_|___/_| |_|\__,_|_|_(_)___/
-//  \____/                                       
+//  \____/
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll                    long long int
-#define pb                    push_back
-#define vi                    vector<int>
-#define vll                   vector<ll>
-#define vc                    vector<char>
+#define ll long long int
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vc vector<char>
 #define fo(i, n) for (long long i = 0; i < n; i++)
 #define s(a) a.size()
-#define c(out) cout<<out<<endl
+#define c(out) cout << out << endl
 
-/*---------------------------------------------------------------CODE BEGINS ->!---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------CODE BEGINS
+ * ->!---------------------------------------------------------------------------*/
 
-void _code(){
-
+int gcd(int a, int b) {
+  if (a == 0) {
+    return b;
+  } else
+    return gcd(b % a, a);
 }
 
-int main(){
-    ll t;
-    cin>>t;
-    while(t--) {
-        _code();
-    }
-}
+int main() {
+	int n,m,x,y,a,b;
+	cin>>n>>m>>x>>y>>a>>b;
+	int g = gcd(a,b);
+	a/=g;
+	b/=g;
+
+	int scale = min(n/a,m/b),
+		x0 = 0, y0 = 0,
+		xn = scale*a, ym = scale*b;
+	x0 = x-xn/2 - xn%2;
+	y0 = y-ym/2 - ym%2;
+	if (x0<0) x0=0;
+	if (y0<0) y0=0;
+	if (x0+xn>n)x0 = n - xn;
+	if (y0 +ym>m)y0 = m - ym;
+	cout<<x0<<" "<<y0<<" "<<x0+xn<<" "<<y0+ym<<endl;
+  }
